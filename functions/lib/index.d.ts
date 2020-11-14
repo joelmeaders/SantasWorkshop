ae6cb3b330180632fd84072a1bf253c4f7b1eb2e
Joel Meaders <joelmeaders@gmail.com>
2020-11-14 13:58:17 -0700

    project move/reconfig 1

--- functions/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="express" />
-import * as functions from 'firebase-functions';
-export declare const trackCollectionMeta: functions.CloudFunction<functions.Change<functions.firestore.DocumentSnapshot>>;
-export declare const modCounter: functions.TriggerAnnotated & ((req: functions.Request<import("express-serve-static-core").ParamsDictionary>, resp: functions.Response<any>) => void | Promise<void>) & functions.Runnable<any>;
-export declare const generateQrCode: functions.CloudFunction<functions.Change<functions.firestore.DocumentSnapshot>>;
-export declare const sendRegistrationEmail2: functions.CloudFunction<functions.Change<functions.firestore.DocumentSnapshot>>;

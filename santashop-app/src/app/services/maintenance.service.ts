40b818d7520e7af8ee7a5fde4f8029d8989f63ed
Joel Meaders <joelmeaders@gmail.com>
2020-11-26 18:48:08 -0700

    Foolproof maintenance mode

--- santashop-app/src/app/services/maintenance.service.ts
+++ santashop-app/src/app/services/maintenance.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { filter, map, mergeMap, take } from 'rxjs/operators';
+import { Router, Event as NavigationEvent, NavigationEnd } from '@angular/router';
+import { filter, map, publishReplay, refCount, switchMap, take } from 'rxjs/operators';
 import { AuthService, FireCRUDStateless } from 'santashop-core/src/public-api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MaintenanceService {
-
   private readonly PATH = 'parameters';
   private readonly DOCID = 'public';
 
-  private readonly $publicParams =
-    this.httpService.readOne(this.PATH, this.DOCID).pipe(
-      take(1)
-    );
+  private readonly $publicParams = this.httpService
+    .readOne(this.PATH, this.DOCID)
+    .pipe(take(1));
 
   public readonly $isMaintenance = this.$publicParams.pipe(
-    map((response: any) => response.maintenance as boolean)
+    map((response: any) => response.maintenance as boolean),
+    publishReplay(1),
+    refCount()
   );
 
   public readonly $messageEn = this.$publicParams.pipe(
@@ -28,15 +28,19 @@ export class MaintenanceService {
     map((response: any) => response.esnotification as string)
   );
 
-  public readonly subscription = this.$isMaintenance.pipe(
-    filter((response: boolean) => !!response),
-    mergeMap(() => this.authService.$IsAdminNoAuth),
-    filter((response: boolean) => !response)
-  ).subscribe(() => this.router.navigate(['/maintenance']));
+  // Force maintenance mode to activate when on
+  public readonly navigationSub = this.router.events.pipe(
+      filter((event: NavigationEvent) => event instanceof NavigationEnd),
+      filter((event: NavigationEnd) => event.url !== '/maintenance'),
+      switchMap(() => this.$isMaintenance),
+      filter((response: boolean) => !!response),
+      switchMap(() => this.authService.$IsAdminNoAuth),
+      filter((response: boolean) => !response),
+    ).subscribe(() => this.router.navigate(['/maintenance']));
 
   constructor(
     private readonly httpService: FireCRUDStateless,
     private readonly authService: AuthService,
     private readonly router: Router
-  ) { }
+  ) {}
 }

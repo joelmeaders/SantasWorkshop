faa87ee07ac68a0d005c80b15717eb198ef4ea42
Joel Meaders <joelmeaders@gmail.com>
2020-11-26 19:24:17 -0700

    Routing import fix

--- santashop-admin/src/app/app-routing.module.ts
+++ santashop-admin/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from 'santashop-app/src/app/core/guards/auth.guard';
 import { AdminGuard } from './guards/admin.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {

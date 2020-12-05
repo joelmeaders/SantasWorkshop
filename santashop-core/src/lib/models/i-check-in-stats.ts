a81d6b3585a50ff48868be7a4c29563b60f00f36
Joel Meaders <joelmeaders@gmail.com>
2020-12-05 14:39:40 -0700

    Scheduled check-in stats function

---
+++ santashop-core/src/lib/models/i-check-in-stats.ts
@@ -0,0 +1,13 @@
+export interface ICheckInAggregatedStats {
+  lastUpdated: Date;
+  dateTimeCount: ICheckInDateTimeCount[];
+}
+
+export interface ICheckInDateTimeCount {
+  date: number;
+  hour: number;
+  customerCount: number;
+  childCount: number;
+  pregisteredCount: number;
+  modifiedCount: number;
+}

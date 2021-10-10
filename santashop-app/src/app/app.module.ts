feb5f7bb8cb66c747f9c2c4780924ce534418197
Joel Meaders <joelmeaders@gmail.com>
2021-10-09 22:35:43 -0600

    One more

--- santashop-app/src/app/app.module.ts
+++ santashop-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, isDevMode, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import {
   AngularFireAnalytics,

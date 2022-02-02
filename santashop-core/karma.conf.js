a9035628c96b904410fa770e20c9b6288e973678
Joel Meaders <joelmeaders@gmail.com>
2022-02-01 20:55:48 -0700

    Enable code coverage reports

--- santashop-core/karma.conf.js
+++ santashop-core/karma.conf.js
@@ -20,7 +20,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcov' }
       ]
     },
     reporters: ['progress', 'kjhtml'],

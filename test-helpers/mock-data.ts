4123f9e10986eecc099fd35dbc28996ca8942a8b
Joel Meaders <joelmeaders@gmail.com>
2022-07-06 23:10:50 -0600

    mockData

--- test-helpers/mock-data.ts
+++ test-helpers/mock-data.ts
@@ -1,5 +1,10 @@
 import { Timestamp } from 'firebase/firestore';
-import { IRegistration } from '../dist/santashop-models';
+import {
+	AgeGroup,
+	IChild,
+	IRegistration,
+	ToyType,
+} from '../santashop-models/src';
 
 export const mockRegistrations = (id?: string) => ({
 	complete: {
@@ -32,3 +37,52 @@ export const mockRegistrations = (id?: string) => ({
 		} as IRegistration,
 	},
 });
+
+export const mockChildren = {
+	valid: {
+		infant: {
+			id: 1,
+			firstName: 'John',
+			lastName: 'Doe',
+			dateOfBirth: new Date(),
+			ageGroup: AgeGroup.age02,
+			toyType: ToyType.infant,
+			programYearAdded: new Date().getFullYear(),
+			enabled: true,
+			error: undefined,
+		} as IChild,
+		age35: {
+			id: 2,
+			firstName: 'Jane',
+			lastName: 'Doe',
+			dateOfBirth: new Date(new Date().getFullYear() - 4, 2, 1),
+			ageGroup: AgeGroup.age35,
+			toyType: ToyType.girl,
+			programYearAdded: new Date().getFullYear(),
+			enabled: true,
+			error: undefined,
+		} as IChild,
+		age68: {
+			id: 3,
+			firstName: 'Jesse',
+			lastName: 'Doe',
+			dateOfBirth: new Date(new Date().getFullYear() - 7, 2, 1),
+			ageGroup: AgeGroup.age68,
+			toyType: ToyType.boy,
+			programYearAdded: new Date().getFullYear(),
+			enabled: true,
+			error: undefined,
+		} as IChild,
+		age911: {
+			id: 3,
+			firstName: 'Jesse',
+			lastName: 'Doe',
+			dateOfBirth: new Date(new Date().getFullYear() - 10, 2, 1),
+			ageGroup: AgeGroup.age911,
+			toyType: ToyType.girl,
+			programYearAdded: new Date().getFullYear(),
+			enabled: true,
+			error: undefined,
+		} as IChild,
+	},
+};

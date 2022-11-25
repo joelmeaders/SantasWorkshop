26b180f09c103314b963206b9e0ed0feaeed059c
Joel Meaders <joelmeaders@gmail.com>
2022-11-25 11:28:11 -0700

    Cleanup

--- santashop-admin/src-old/app/pages/admin/admin.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-admin',
-	templateUrl: 'admin.page.html',
-	styleUrls: ['admin.page.scss'],
-})
-export class AdminPage {}

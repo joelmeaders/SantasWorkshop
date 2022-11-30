1b9791f4530e4de8a7e82477cd71843789b2e0be
Joel Meaders <joelmeaders@gmail.com>
2022-11-29 20:20:45 -0700

    Search functionality and style updates

--- santashop-admin/src/app/pages/admin/search/results/results-routing.module.ts
+++ santashop-admin/src/app/pages/admin/search/results/results-routing.module.ts
@@ -4,14 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ResultsPage } from './results.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ResultsPage
-  }
+	{
+		path: '',
+		component: ResultsPage,
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class ResultsPageRoutingModule {}
--- santashop-admin/src/app/pages/admin/search/search.service.ts
+++ santashop-admin/src/app/pages/admin/search/search.service.ts
@@ -2,13 +2,18 @@ import { Injectable } from '@angular/core';
 import { FireRepoLite, QueryConstraint } from '@core/*';
 import { COLLECTION_SCHEMA, RegistrationSearchIndex } from '@models/*';
 import { limit, orderBy, where } from 'firebase/firestore';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, shareReplay } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class SearchService {
-	public searchResults$: Observable<RegistrationSearchIndex[]> | null = null;
+	private readonly searchResults = new BehaviorSubject<Observable<
+		RegistrationSearchIndex[]
+	> | null>(null);
+	public readonly searchResults$: Observable<Observable<
+		RegistrationSearchIndex[]
+	> | null> = this.searchResults.asObservable().pipe(shareReplay(1));
 
 	private readonly index =
 		this.repoService.collection<RegistrationSearchIndex>(
@@ -20,7 +25,7 @@ export class SearchService {
 		zipCode: string
 	): QueryConstraint[] =>
 		[
-			where('zip', '==', zipCode),
+			where('zip', '==', zipCode.toString()),
 			where('lastName', '>=', lastName),
 			where('lastName', '<=', lastName + '\uf8ff'),
 			orderBy('lastName', 'asc'),
@@ -41,24 +46,26 @@ export class SearchService {
 	constructor(private readonly repoService: FireRepoLite) {}
 
 	public searchByLastNameZip(lastName: string, zipCode: string): void {
-		this.searchResults$ = this.index.readMany(
-			this.queryLastNameZip(lastName.toLowerCase(), zipCode)
+		this.searchResults.next(
+			this.index.readMany(
+				this.queryLastNameZip(lastName.toLowerCase(), zipCode)
+			)
 		);
 	}
 
 	public searchByEmail(emailAddress: string): void {
-		this.searchResults$ = this.index.readMany(
-			this.queryEmail(emailAddress.toLowerCase())
+		this.searchResults.next(
+			this.index.readMany(this.queryEmail(emailAddress.toLowerCase()))
 		);
 	}
 
 	public searchByCode(code: string): void {
-		this.searchResults$ = this.index.readMany(
-			this.queryCode(code.toUpperCase())
+		this.searchResults.next(
+			this.index.readMany(this.queryCode(code.toUpperCase()))
 		);
 	}
 
 	public reset(): void {
-		this.searchResults$ = null;
+		this.searchResults.next(null);
 	}
 }

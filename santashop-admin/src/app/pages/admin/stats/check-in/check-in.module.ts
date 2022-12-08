f35c4cedd1a116728a51c3871087791d7e06a087
Joel Meaders <joelmeaders@gmail.com>
2022-12-07 17:13:50 -0700

    checkin-stats

--- santashop-admin/src/app/pages/admin/stats/check-in/check-in.module.ts
+++ santashop-admin/src/app/pages/admin/stats/check-in/check-in.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CheckInPageRoutingModule } from './check-in-routing.module';
 
 import { CheckInPage } from './check-in.page';
+import { SharedModule } from '../../../../shared/shared.module';
+import { NgChartsModule } from 'ng2-charts';
+import { CoreModule } from '@core/*';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CheckInPageRoutingModule
-  ],
-  declarations: [CheckInPage]
+	imports: [
+		CommonModule,
+		IonicModule,
+		CoreModule,
+		CheckInPageRoutingModule,
+		SharedModule,
+		NgChartsModule,
+	],
+	declarations: [CheckInPage],
 })
 export class CheckInPageModule {}

c5ef236e3e8446f49c86ded553577a7eb47f1cc4
joelmeaders <joelmeaders@gmail.com>
2023-11-18 13:49:40 -0500

    fix(app/admin): Add polyfills (#100)
    * polyfills
    
    * browserlist in admin

--- polyfills.ts
+++ polyfills.ts
@@ -41,13 +41,14 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
-
+ 
 import './zone-flags';
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js'; // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS

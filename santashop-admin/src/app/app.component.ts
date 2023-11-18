176e8a1f04c4486d1f4acb8999104b9d0734ef27
Joel Meaders <joelmeaders@gmail.com>
2023-11-18 12:08:26 -0700

    dark mode, sign out, disable menu options for now

--- santashop-admin/src/app/app.component.ts
+++ santashop-admin/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-const prefersDark = !!window.matchMedia('(prefers-color-scheme: dark)') ?? true;
-if (prefersDark) document.body.classList.toggle('dark', prefersDark);
-
 @Component({
 	selector: 'admin-root',
 	templateUrl: 'app.component.html',
